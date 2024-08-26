dea268b13ab46066d2b53ecd53ef1325fd2d220a
lam <lam@ninx.xyz>
2024-08-26 23:12:43 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+## PatientEdx
+This is an application built to rival Uptodate and Webmd for providing patients with access to information. the information listed is LLM generated and is separated into disease, treatment of disease and drug details. Supplemenatary information is obtained by scrapping various sites such as drug price sites.
+
+### Stack.
+| Type | Detail |
+|---|---|
+| Language | Common Lisp (SBCL)|
+| Database | Clustered KVRocks |
+| OS | Ubuntu 24.04 |
+
+### Monetisation.
+The app is going to be monetised by ads from Google adsense. We have to look for ways of driving up traffic to the site.
---
+++ src/packages.lisp
@@ -0,0 +1,32 @@
+(defpackage :patientedx.nlp
+  (:use :cl :str :trivia)
+  (:nicknames :nlp)
+  (:shadow str:match)
+  (:documentation "This package for processing text")
+  (:export :remove-json-encapsulation :remove-lisp-encapsulation :make-strict-json :tokenize :count-terms :compute-tf))
+
+(defpackage :patientedx.apis
+  (:use :cl :drakma :com.inuoe.jzon)
+  (:nicknames :llms)
+  (:documentation "This package for making rest api calls to llm endpoints")
+  (:local-nicknames (:jzon :com.inuoe.jzon))
+  (:export :make-gemini-multiturns
+   :query-gemini
+	   :query-together
+	   :query-groq
+   :query-ollama
+	   :query-azure-ai
+	   *index-prompt*
+	   *system-prompt*
+	   *sexp-index-prompt*
+	   *sexp-title-prompt*
+	   *json-chat-prompt*
+	   *default-together-model*
+	   *qwen-system-prompt*))
+
+(defpackage :patientedx
+  (:use :cl :redis :easy-routes :hunchentoot :cl-who :cl-css :cl-base64 :frugal-uuid :trivia :com.inuoe.jzon :parenscript :local-time)
+  (:shadow easy-routes:redirect hunchentoot:reply parenscript:@ parenscript:stringify parenscript:% redis:close-connection cl-who:fmt redis:tell str:match)
+  (:documentation "The main package of the patientedx application.")
+  (:local-nicknames (:jzon :com.inuoe.jzon))
+  (:export :start-server :restart-server :create-tables))
