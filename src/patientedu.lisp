c88c5aaa09db90bcbdd202ef6d1620f0286f75dd
lam <lam@ninx.xyz>
2024-08-28 17:01:18 +0300

    add a minor improvement to the code

--- src/patientedu.lisp
+++ src/patientedu.lisp
@@ -4,7 +4,10 @@
   "create a prompt to get disease details."
   (format nil "You will act as a patient educator who communicates in common lisp. You will generate a summary, cause, epidemiology, signs and symptoms, compilcations, differential diagnoses of ~a. The data should be detailed, but when you don't know a thing, that will be nil. Return a plist (:introduction ... :cause ... :risk-factors (...) :epidemiology ... :diagnosis (...) :pathophysiology ... :signs-and-symptoms (...) :complications (...) :differential-diagnoses (...) :alternative-names (...) :prevention () :living-with (...)). The data is to be used in blog posts so act accordingly. 
 
-Example of desired output: (:introduction \"Introduce the disease\" :cause \"Detailed disease cause\" :epidemiology \"Detailed disease epidemiology, qouting the latest statistics you have access too. Don't make numbers up.\" :risk-factors (\"Risk factor 1\" ...) :diagnosis (\"How disease is diagnosed 1\" ...) :pathophysiology \"A few details about how the process occurs\" :signs-and-symptoms (\"Sign 1\" ...) :complications (\"Complication with a brief explanation of how it comes about\" ...) :differential-diagnosis (\"Ddx 1\" ...) :alternative-names (\"Other name of disease 1\" ...) :prevention (\"Way 1\" ...) :living-with (\"Way 1\" ...)) " disease))
+Example of desired output: (:introduction \"Introduce the disease\" :cause \"Detailed disease cause\" :epidemiology \"Detailed disease epidemiology, qouting the latest statistics you have access too. Don't make numbers up.\" :risk-factors (\"Risk factor 1\" ...) :diagnosis (\"How disease is diagnosed 1\" ...) :pathophysiology \"A few details about how the process occurs\" :signs-and-symptoms (\"Sign 1\" ...) :complications (\"Complication with a brief explanation of how it comes about\" ...) :differential-diagnosis (\"Ddx 1\" ...) :alternative-names (\"Other name of disease 1\" ...) :prevention (\"Way 1\" ...) :living-with (\"Way 1\" ...))
+
+If you don't have any data about the disease: return nil (as in common lisp's nil).
+" disease))
 
 (defun get-disease-details (disease)
   "makes a call to the best meodel we have right now, now we will be using sexp to get the data, the data will be returned as plists.
